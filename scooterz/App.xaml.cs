5b27dfa14b09677755f8dffecd12aad0cd4cd22c
Chiril Gorbenco <chiril.gorbenco@student.upt.ro>
2023-10-28 10:20:34 +0300

    Add project files.

---
+++ scooterz/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace scooterz
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ scooterz/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace scooterz
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
